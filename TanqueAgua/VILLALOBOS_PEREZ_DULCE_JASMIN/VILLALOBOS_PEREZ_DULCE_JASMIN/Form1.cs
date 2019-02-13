0f1a8f7462e179246a65abc4bffd55f6105db254
DULCE VILLALOBOS <42878496+Villalobos39@users.noreply.github.com>
2019-02-12 16:43:01 -0800

    ACT

---
+++ TanqueAgua/VILLALOBOS_PEREZ_DULCE_JASMIN/VILLALOBOS_PEREZ_DULCE_JASMIN/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VILLALOBOS_PEREZ_DULCE_JASMIN
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int progreso = 0; 
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (progreso>=0 && progreso<=5000)
+            {
+                progreso += 100;
+                Bnivel.Value = progreso;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (progreso >= 1 && progreso <= 5000)
+            {
+                progreso -= 100;
+                Bnivel.Value = progreso;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (progreso >= 0 && progreso <= 5000)
+            {
+                progreso += 50;
+                Bnivel.Value = progreso;
+            }
+        }
+
+        private void textBox1_AcceptsTabChanged(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = progreso.ToString();
+        }
+
+      
+    }
+}
